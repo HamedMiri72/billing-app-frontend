e8fe58e62106c224c5d5822173934892fcab655b
hamedmiri <hamedmiri72@gmail.com>
2025-06-26 01:13:41 +0100

    service to fetch apis

---
+++ src/service/CategoryService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+export const addCategory = async (category) => {
+
+    return await axios.post("http://localhost:9000/api/v1.0/categories/add", category);
+}
+
+export const getAllCategories = async () => {
+    return await axios.get("http://localhost:9000/api/v1.0/categories/");
+}
+
+export const deleteCategory = async (categoryId) => {
+    return await axios.delete(`http://localhost:9000/api/v1.0/categories/delete/${categoryId}`);
+}
