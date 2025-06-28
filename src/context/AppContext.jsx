66c8ec65ff93c593dc82463247e72fbad1833cc1
hamedmiri <hamedmiri72@gmail.com>
2025-06-28 11:31:39 +0100

    app context provider

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { createContext } from 'react';
+
+
+
+
+export const AppContext = createContext(null);
+
+export const AppContextProvider = (props) => {
+
+
+    const contextValue = {
+
+    }
+
+    return (
+        <AppContext.Provider value={contextValue}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
